eb502cba501dfd3cef0a8ff39c8bd0ee9c0c872d
Gary Lawler <LawleGa@aerlingus.ie>
2018-07-05 13:48:40 +0100

    add a simple REST endpoint that returns the string "ok"

---
+++ src/main/java/com/glawler/demo/config/ApplicationConfig.java
@@ -0,0 +1,11 @@
+package com.glawler.demo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.glawler.demo.controller")
+public class ApplicationConfig {
+}
