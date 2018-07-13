141f4ca7ee886430f0f98438b904f3256b9c2fbf
Gary Lawler <LawleGa@aerlingus.ie>
2018-07-13 15:40:32 +0100

    introduce Redis

--- src/main/java/com/glawler/demo/config/ApplicationConfig.java
+++ src/main/java/com/glawler/demo/config/ApplicationConfig.java
@@ -1,11 +1,35 @@
 package com.glawler.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.net.UnknownHostException;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.glawler.demo.controller")
 public class ApplicationConfig {
+
+  @Bean
+  @Lazy
+  public RedisConnectionFactory defaultRedisConnectionFactory() throws UnknownHostException {
+    RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration("redis", 6379);
+    return new LettuceConnectionFactory(configuration);
+  }
+
+  @Bean
+  @Lazy
+  public ValueOperations getStringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    StringRedisTemplate redisTemplate = new StringRedisTemplate();
+    redisTemplate.setConnectionFactory(redisConnectionFactory);
+    return redisTemplate.opsForValue();
+  }
 }
--- src/main/java/com/glawler/demo/controller/DemoController.java
+++ src/main/java/com/glawler/demo/controller/DemoController.java
@@ -1,13 +1,18 @@
 package com.glawler.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+  @Autowired
+  public ValueOperations valueOperations;
+
   @RequestMapping("/")
-  public String getBase() {
-    return "ok";
+  public Long getCount() {
+    return valueOperations.increment("visits", 1);
   }
 }
